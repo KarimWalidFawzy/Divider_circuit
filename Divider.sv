d77b3855aa2753559e7a7fb956bc7bad209b1a18
KarimWalidFawzy <132375356+KarimWalidFawzy@users.noreply.github.com>
2024-06-16 01:01:47 +0300

    Update Divider.sv

--- Divider.sv
+++ Divider.sv
@@ -7,11 +7,15 @@ module divider(
     output DZ);
     reg [PRECISION-1:0] C;
     reciprocal R(B,C,DZ);
-    reg [(ARG_BIT_WIDTH-1)+(PRECISION):0] Temp;
+    reg [ARG_BIT_WIDTH+PRECISION-1:0] Temp;
     always @(*) begin
         if (!DZ) begin
-            Temp<= ((96'b0+C)*(96'b0+A));
+            Temp<= ((96'b0+C)*(96'b0+A));//(PRECISION+ARG_BIT_WIDTH)'b0
         end
     end
     assign dividend_and_fraction = Temp;
 endmodule
+/*
+dividend[PRECISION-1:0]=fraction
+dividend[ARG_BIT_WIDTH-1:PRECISION]=integer
+*/
