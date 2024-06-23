94753b110e37b56255524f759b48b59f8af3fba2
KarimWalidFawzy <132375356+KarimWalidFawzy@users.noreply.github.com>
2024-06-23 20:18:59 +0300

    Update subandshft.sv

--- Combinational version/subandshft.sv
+++ Combinational version/subandshft.sv
@@ -12,23 +12,3 @@ module subandshft(
     assign nn=~(diff<0);
     assign c=nn?(diff<<1):(a<<1);
 endmodule
-/*
-a=[]
-b=[]
-a= 010110 | 22-10=12=1100
-b= 001010 | ~b=110101 (~b+1)= 110110
----------
-   010110
-   110110
----------
-(1)001100 ignore the one -considered overflow-
-
-a+~b+1==(a+1)+~b
-x[n:0];
-x<<1+1=={x[n-1:0],1};
-concatenate by 1 or concatenate by 0 
-a[31]=1
-
-
-
-*/
